20e4655e182edd5dc6f21da654f8941334cde655
McNee1 <ulanov.i@mail.ru>
2024-04-02 12:05:16 +0300

    ci: Initial

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ export const App = () => {
   return (
     <>
       <Header />
+      <div className='container'>
+        <div className='max-w-[800px] m-auto'>hello</div>
+      </div>
     </>
   );
 };
