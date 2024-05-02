c8e6044a5987625017abf8c36fdf3204bfe99860
McNee1 <ulanov.i@mail.ru>
2024-05-02 15:41:40 +0300

    style: Correct Skeleton

--- src/shared/ui/skeleton/Skeleton.tsx
+++ src/shared/ui/skeleton/Skeleton.tsx
@@ -2,10 +2,10 @@ export const Skeleton = () => {
   return (
     <>
       <div
-        className='animate-pulse p-4 md:p-6 dark:border-gray-700'
+        className='relative animate-pulse dark:border-gray-700'
         role='status'
       >
-        <div className='mb-4 flex h-48 items-center justify-center rounded bg-gray-300 dark:bg-gray-700'>
+        <div className='mb-16 flex h-48 items-center justify-center rounded bg-gray-300 dark:bg-gray-700'>
           <svg
             className='h-10 w-10 text-gray-200 dark:text-gray-600'
             xmlns='http://www.w3.org/2000/svg'
@@ -17,28 +17,30 @@ export const Skeleton = () => {
             <path d='M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z' />
           </svg>
         </div>
-        <div className='mb-5 flex items-center'>
-          <svg
-            className='me-3 h-14 w-14 text-gray-200 dark:text-gray-700'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='currentColor'
-            viewBox='0 0 20 20'
-            aria-hidden='true'
-          >
-            <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z' />
-          </svg>
-          <div>
-            <div className='mb-2 h-2.5 w-32 rounded-full bg-gray-200 dark:bg-gray-700'></div>
-            <div className='h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700'></div>
+        <div className='container max-w-[800px]'>
+          <div className='absolute top-[40%] flex items-center'>
+            <svg
+              className='me-3 h-20 w-20 text-gray-200 dark:text-gray-700'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              aria-hidden='true'
+            >
+              <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z' />
+            </svg>
+            {/* <div>
+              <div className='mb-2 h-2.5 w-32 rounded-full bg-gray-200 dark:bg-gray-700'></div>
+              <div className='h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700'></div>
+            </div> */}
           </div>
-        </div>
-        <div className='mb-4 h-2.5 w-1/4 rounded-full bg-gray-200 dark:bg-gray-700'></div>
-        <div className='mb-5 h-3 w-1/3 rounded-full bg-gray-200 dark:bg-gray-700'></div>
-        <div className='mb-4 h-3.5 rounded-full bg-gray-200 dark:bg-gray-700'></div>
-        <div className='mb-4 h-4 rounded-full bg-gray-200 dark:bg-gray-700'></div>
-        <div className='h-5 w-1/2  rounded-full bg-gray-200 dark:bg-gray-700'></div>
+          <div className='mb-4 h-3.5 w-1/4 rounded-full bg-gray-200 dark:bg-gray-700'></div>
+          <div className='mb-5 h-3 w-1/3 rounded-full bg-gray-200 dark:bg-gray-700'></div>
+          <div className='mb-4 h-3.5 rounded-full bg-gray-200 dark:bg-gray-700'></div>
+          <div className='mb-4 h-4 rounded-full bg-gray-200 dark:bg-gray-700'></div>
+          <div className='h-5 w-1/2  rounded-full bg-gray-200 dark:bg-gray-700'></div>
 
-        <span className='sr-only'>Loading...</span>
+          <span className='sr-only'>Loading...</span>
+        </div>
       </div>
     </>
   );
